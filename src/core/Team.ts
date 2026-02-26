96db976bb58f9c7f1220106952cb8ac7123eaa33
Elshad Aghayev <3605068+elshadaghazade@users.noreply.github.com>
2026-02-26 19:59:26 +0100

    added Match class tests

--- src/core/Team.ts
+++ src/core/Team.ts
@@ -4,6 +4,9 @@ export class Team {
     constructor(public name: string) {}
 
     setScore(score: number) {
+        if (score < 0) {
+            throw new Error("score must be a non-negative integer");
+        }
         this.score = score;
     }
 
---
+++ tests/match.test.ts
@@ -0,0 +1,11 @@
+import { describe, test, expect } from '@jest/globals';
+import { Team } from '../src/core/Team';
+import { Match } from '../src/core/Match';
+
+describe('Match', () => {
+    test('initially match is not ongoing, not finished, no timestamps', () => {
+        const home = new Team('Home');
+        const away = new Team('Away');
+        
+    });
+});
