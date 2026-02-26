7689769fbfbc04cef7d244853221e9aedc62e414
Elshad Aghayev <3605068+elshadaghazade@users.noreply.github.com>
2026-02-26 17:12:59 +0100

    added scoreboard important methods but they are unimplemented yet

--- src/core/Scoreboard.ts
+++ src/core/Scoreboard.ts
@@ -1,5 +1,19 @@
 import { EventEmitter } from "node:stream";
 
 export class Scoreboard extends EventEmitter {
+    startNewMatch () {
 
+    }
+
+    updateScore () {
+
+    }
+
+    finish () {
+
+    }
+
+    getSummary () {
+        
+    }
 }
