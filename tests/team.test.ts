547a41d7e5effbc87b32df0a87cd3d9064580213
Elshad Aghayev <3605068+elshadaghazade@users.noreply.github.com>
2026-02-26 19:55:27 +0100

    Team's test cases added

--- tests/team.test.ts
+++ tests/team.test.ts
@@ -4,7 +4,22 @@ import { Team } from '../src/core/Team';
 describe('Team', () => {
     test('stores the name from constructor', () => {
         const team = new Team('Arsenal');
-
         expect(team.name).toBe('Arsenal');
+    })
+
+    test('starts with score = 0', () => {
+        const team = new Team('Arsenal');
+        expect(team.getScore()).toBe(0);
+    })
+
+    test('setScore updates the score', () => {
+        const team = new Team('Arsenal');
+        team.setScore(7);
+        expect(team.getScore()).toBe(7);
     });
+
+    test('setScore throws error on negative score', () => {
+        const team = new Team('Arsenal');
+        expect(() => team.setScore(-1)).toThrow("score must be a non-negative integer");
+    })
 });
