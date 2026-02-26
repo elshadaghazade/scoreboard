95708eb3bc27ab62dba0e6a240f6ca0ea2e4692f
Elshad Aghayev <3605068+elshadaghazade@users.noreply.github.com>
2026-02-26 20:11:18 +0100

    scoreboard test added

---
+++ tests/scoreboard.ts
@@ -0,0 +1,8 @@
+import { describe, test, expect } from '@jest/globals';
+import { Team } from '../src/core/Team';
+import { Match } from '../src/core/Match';
+import { Scoreboard } from '../src/core/Scoreboard';
+
+describe('Scoreboard', () => {
+    
+})
