80a652b2857f2ea0b6a53fd48a55c7035b52f560
Elshad Aghayev <3605068+elshadaghazade@users.noreply.github.com>
2026-02-26 17:59:18 +0100

    match class was implemented fully

--- src/core/Match.ts
+++ src/core/Match.ts
@@ -1,3 +1,45 @@
+import type { Team } from "./Team";
+
 export class Match {
-    
+
+    private startedAt?: number;
+    private finishedAt?: number;
+
+    constructor(
+        public id: string | number,
+        public homeTeam: Team,
+        public awayTeam: Team
+    ) {}
+
+    start(timestamp: number) {
+        if (this.startedAt || this.finishedAt) {
+            return;
+        }
+
+        this.startedAt = timestamp;
+    }
+
+    finish(timestamp: number) {
+        if (!this.startedAt || this.finishedAt) {
+            return;
+        }
+
+        this.finishedAt = timestamp;
+    }
+
+    isOngoing () {
+        return !!this.startedAt && !this.finishedAt;
+    }
+
+    isFinished () {
+        return !!this.startedAt && !!this.finishedAt;
+    }
+
+    whenStarted () {
+        return this.startedAt;
+    }
+
+    whenFinished () {
+        return this.finishedAt;
+    }
 }
